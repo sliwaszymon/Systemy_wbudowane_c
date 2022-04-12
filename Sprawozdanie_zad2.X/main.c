242da81bdaf40100cc2af3ee87fe292cf944bd29
Szymon Sliwa <szymon242820@gmail.com>
2022-04-12 11:11:41 +0200

    zad 2

---
+++ Sprawozdanie_zad2.X/main.c
@@ -0,0 +1,177 @@
+// CONFIG1H
+#pragma config OSC = HSPLL      // Oscillator Selection bits (HS oscillator, PLL enabled (Clock Frequency = 4 x FOSC1))
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config PWRT = ON        // Power-up Timer Enable bit (PWRT enabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
+#pragma config BORV = 3         // Brown Out Reset Voltage bits (Minimum setting)
+
+// CONFIG2H
+#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+
+// CONFIG3H
+#pragma config CCP2MX = PORTC   // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = OFF     // PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+#pragma config LPT1OSC = OFF    // Low-Power Timer1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config MCLRE = ON       // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+
+// CONFIG4L
+#pragma config STVREN = OFF     // Stack Full/Underflow Reset Enable bit (Stack full/underflow will not cause Reset)
+#pragma config LVP = OFF        // Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000800-003FFFh) not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (004000-007FFFh) not code-protected)
+#pragma config CP2 = OFF        // Code Protection bit (Block 2 (008000-00BFFFh) not code-protected)
+#pragma config CP3 = OFF        // Code Protection bit (Block 3 (00C000-00FFFFh) not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000800-003FFFh) not write-protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (004000-007FFFh) not write-protected)
+#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (008000-00BFFFh) not write-protected)
+#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (00C000-00FFFFh) not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block (000000-0007FFh) not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000800-003FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (004000-007FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (008000-00BFFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (00C000-00FFFFh) not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF
+
+#include <xc.h>
+
+int direction = 1;
+
+void delay(unsigned int ms)
+{
+    unsigned int i;
+    unsigned char j;
+    
+ for (i =0; i< ms; i++)
+ {
+ 
+  for (j =0 ; j < 200; j++)
+   {
+      Nop();
+      Nop();
+      Nop();
+      Nop();
+      Nop();
+   }
+ }
+}
+
+unsigned int adc(unsigned char kanal)
+{
+    switch(kanal)
+    {
+        case 0: ADCON0=0x01; break; //P1
+        case 1: ADCON0=0x05; break; //P2
+        case 2: ADCON0=0x09; break; 
+    }
+    
+    ADCON0bits.GO=1;
+    while(ADCON0bits.GO == 1);
+
+   return ((((unsigned int)ADRESH)<<2)|(ADRESL>>6));
+}
+
+void better_delay(unsigned int value) {
+    if (value < 51) { 
+        delay(0);
+    } else if (value >= 55 && value < 102) {
+        delay(200);
+    } else if (value >= 102 && value < 153) {
+        delay(400);
+    } else if (value >= 153 && value < 204) {
+        delay(800);
+    } else {
+        delay(1600);
+    }
+}
+
+unsigned char bin_up(unsigned char display) {
+    better_delay(((unsigned int)adc(1)/10));
+    return (display + 1);
+}
+
+unsigned char snake(unsigned char display){
+    if (display < 7) {
+		display = (display << 1) ^ 1;
+	} else {
+		if (direction == 1) {
+            if (display == 224) {
+				direction = 0;
+				display = display >> 1;
+            } else {
+				display = display << 1; 	
+			}
+		} else {
+			if (display == 7) {
+				direction = 1;
+				display = display << 1;
+			} else {
+				display = display >>1;
+			}
+		}
+	}
+    better_delay(((unsigned int)adc(1)/10));
+    return display;
+}
+
+void main(void) {
+    
+    //Inicjalizacja konwertera analogowo cyfrowego
+    ADCON0=0x01;
+    ADCON1=0x0B;
+    ADCON2=0x01;
+    
+    TRISA=0xC3;
+    TRISB=0x3F;   
+    TRISC=0x01;
+    TRISD=0x00;
+    TRISE=0x00;
+    
+    unsigned char display = 0;
+    unsigned int tmp = 0;
+    
+    while(1)
+    {
+        PORTD = display; //Wyswietlenie na diodach rejestru D
+        unsigned int task = 1;
+        delay(100);
+        if (PORTBbits.RB3 == 0) {
+            task = 1;
+            display = 0;
+        } else if (PORTBbits.RB4 == 0) {
+            task = 2;
+            display = 0;
+            direction = 1;
+        }
+        
+        if (task == 1) {
+            display = bin_up(display);
+        } else if (task == 2) {
+            display = snake(display);
+        }
+    }
+    
+    return;
+}
+
+//warto?ci na potencjometrze w zakresie oko?o (0, 1020)
